770bda0047caf6900e3656e29d1be5ce9abf15e5
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-02-11 11:45:28 -0500

    Create F# project start

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -1,3 +1,25 @@
 ﻿open Hangman
+open System
+open System.IO
 
-printfn "%s" Hangman.deadMan
+let greetUser = 
+    printfn "\n\
+    Welcome to Hangman! We have chosen a random word for you.\n\
+    Let's start by choosing a letter: \n\
+    "
+
+let chooseRandomWord = 
+    let rnd = Random()
+    let words = "../words.txt" |> File.ReadAllLines
+    let randomWord = words |> Array.item (rnd.Next(words.Length))
+    randomWord
+
+let start = 
+    // I eventually want chooseRandomWord here
+    greetUser
+
+[<EntryPoint>]
+let main argv = 
+    let randomWord = chooseRandomWord
+    start
+    0
