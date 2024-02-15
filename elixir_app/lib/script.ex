98751ca423f1e7cc5f78d0b1c4bb4d957f92727d
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-02-15 13:35:20 -0500

    Implement choose_random_word in Elixir

--- elixir_app/lib/script.ex
+++ elixir_app/lib/script.ex
@@ -1,5 +1,19 @@
-import IO
+defmodule Gameboard do
+    def choose_random_word() do
+        {err, file} = File.read("../words.txt")
+        case err do
+            :ok ->
+                lst = String.split(file)
+                len = Enum.count(lst)
+                num = Enum.random(0..len)
+                Enum.at(lst, num)
+            _ ->
+                "Error reading file"
+        end
+    end
+end
+
 
 defmodule Main do
-
+    IO.puts Gameboard.choose_random_word()
 end
