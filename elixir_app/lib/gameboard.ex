0b46a7a61ec19a7a091b8b8158c27e8bf9024a00
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-02-20 13:50:06 -0500

    Implement guess state logic

--- elixir_app/lib/gameboard.ex
+++ elixir_app/lib/gameboard.ex
@@ -1,4 +1,26 @@
 defmodule GameBoard do
+    def run(pid) do
+        IO.puts GameState.get_word(pid) # for debugging
+
+        guess = IO.gets(:stdio, "Please enter a letter: ") |> String.trim()
+        guess =
+            case guess do
+                c when c != "" -> String.at(c, 0) |> String.downcase
+                "" ->
+                    IO.puts "Invalid input. Defaulting to 'a'"
+                    "a"
+            end
+
+        GameState.guess_letter(pid, guess)
+        GameState.check_last_guess(pid)
+
+        IO.inspect(GameState.get_guesses(pid)) # for debugging
+        game_over = GameState.check_game_over(pid)
+        if game_over == false do
+            run(pid)
+        end
+    end
+
     def choose_random_word() do
         {err, file} = File.read("../words.txt")
         case err do
@@ -29,10 +51,7 @@ end
 defmodule GameState do
     use GenServer
 
-    def init(init_state) do
-        {:ok, init_state}
-    end
-
+    # Public api
     def start_link() do
         GenServer.start_link(__MODULE__, %{
             word: choose_random_word(),
@@ -41,26 +60,24 @@ defmodule GameState do
         })
     end
 
+    def choose_random_word() do
+        GameBoard.choose_random_word()
+    end
+
+    def check_game_over(pid) do
+        GenServer.call(pid, :check_game_over)
+    end
+
+    def check_last_guess(pid) do
+        GenServer.call(pid, :check_last_guess)
+    end
+
     def guess_letter(pid, letter) do
         GenServer.cast(pid, {:guess_letter, letter})
     end
 
-    def handle_cast({:guess_letter, letter}, state) do
-        new_guesses = [letter | state.guesses]
-        new_state = %{state | guesses: new_guesses}
-        {:noreply, new_state}
-    end
-
-    def check_num_wrong(pid) do
-        GenServer.call(pid, :check_num_wrong)
-    end
-
-    def handle_call(:check_num_wrong, _from, state) do
-        {:reply, state.wrong_guesses, state}
-    end
-
-    def choose_random_word() do
-        GameBoard.choose_random_word()
+    def check_wrong_guesses(pid) do
+        GenServer.call(pid, :check_wrong_guesses)
     end
 
     def get_word(pid) do
@@ -69,6 +86,59 @@ defmodule GameState do
 
     def get_guesses(pid) do
         GenServer.call(pid, :get_guesses)
+    end
+
+    # GenServer callbacks
+    def init(init_state) do
+        {:ok, init_state}
+    end
+
+    def handle_cast({:guess_letter, letter}, state) do
+        new_guesses = [letter | state.guesses]
+        new_state = %{state | guesses: new_guesses}
+        {:noreply, new_state}
+    end
+
+    def handle_call(:check_game_over, _from, state) do
+        wrong_guesses = state.wrong_guesses
+        random_word = state.word
+        guesses = state.guesses
+        correct_guesses = Enum.filter(guesses, fn (guess) ->
+            String.contains?(random_word, guess)
+        end)
+        cond do
+            wrong_guesses >= 7 ->
+                IO.puts "Game over. The word was #{random_word}"
+                {:reply, true, state}
+            Enum.count(correct_guesses) == String.length(random_word) ->
+                IO.puts "You win! The word was #{random_word}"
+                {:reply, true, state}
+            true ->
+                {:reply, false, state}
+        end
+    end
+
+    def handle_call(:get_state, _from, state) do
+        {:reply, state, state}
+    end
+
+    def handle_call(:check_last_guess, _from, state) do
+        random_word = state.word
+        last_guess = hd(state.guesses)
+        IO.puts last_guess
+        contains = String.contains?(random_word, last_guess)
+        IO.puts contains
+        if contains do
+            {:reply, true, state}
+        else
+            new_wrong_guesses = state.wrong_guesses + 1
+            new_state = %{state | wrong_guesses: new_wrong_guesses}
+            {:reply, false, new_state}
+        end
+    end
+
+    def handle_call(:check_wrong_guesses, _from, state) do
+        {:reply, state.wrong_guesses, state}
     end
 
     def handle_call(:get_word, _from, state) do
--- elixir_app/lib/script.ex
+++ elixir_app/lib/script.ex
@@ -1,14 +1,8 @@
 defmodule Main do
     {err, pid} = GameState.start_link()
     if err == :ok do
-        # main logic
-        IO.puts GameState.get_word(pid)
-        GameState.guess_letter(pid, 'q')
-        IO.inspect GameState.get_guesses(pid)
-        IO.puts GameState.check_num_wrong(pid)
-
+        GameBoard.run(pid)
     else
         IO.puts "Unable to start game."
     end
-
 end
