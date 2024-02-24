029e821e9b8a26ed73d35436a30f8d285b0f795e
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-02-24 10:39:56 -0500

    Refactor choose_random_word()

--- rust/src/gameboard.rs
+++ rust/src/gameboard.rs
@@ -1,10 +1,10 @@
-use std::fs;
-use rand::Rng;
+use std::fs::{read_to_string};
+use rand::{Rng, thread_rng};
 
 pub fn choose_random_word() -> String {
     let path = "../words.txt";
-    let contents = fs::read_to_string(path).expect("Error reading file.");
+    let contents = read_to_string(path).expect("Error reading file.");
     let lines: Vec<&str> = contents.split("\n").collect();
-    let rand_num = ((rand::thread_rng().gen::<u16>()) % lines.len() as u16) as usize;
+    let rand_num = thread_rng().gen_range(0..lines.len());
     String::from(lines[rand_num])
 }
