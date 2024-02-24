5f9101c2b2bf86e642713a31e45325f3b22afd15
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-02-24 16:49:20 -0500

    Implement choose_letter() in Rust

--- rust/src/gameboard.rs
+++ rust/src/gameboard.rs
@@ -1,4 +1,4 @@
-use std::fs::{read_to_string};
+use std::{fs::read_to_string, io::stdin};
 use rand::{Rng, thread_rng};
 
 pub fn choose_random_word() -> String {
@@ -7,4 +7,29 @@ pub fn choose_random_word() -> String {
     let lines: Vec<&str> = contents.split("\n").collect();
     let rand_num = thread_rng().gen_range(0..lines.len());
     String::from(lines[rand_num])
+}
+
+pub fn choose_letter() -> char {
+    println!("Please enter a letter: ");
+    let mut input = String::new();
+    match stdin().read_line(&mut input) {
+        Ok(_) => {
+            let result = input.trim();
+            if !result.is_empty() {
+                result
+                    .chars()
+                    .next()
+                    .unwrap()
+                    .to_ascii_lowercase()
+            }
+            else {
+                println!("Error getting input. Defaulting to 'a'");
+                'a'
+            }
+        },
+        Err(err) => {
+            println!("Error reading input: {}. Defaulting to 'a'", err);
+            'a'
+        }
+    }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,4 +4,10 @@ mod gameboard;
 fn main() {
     let random_word = gameboard::choose_random_word();
     println!("{}", random_word);
+    greet();
+    println!("{}",gameboard::choose_letter())
+}
+
+fn greet() {
+    println!("Welcome to Hangman! We have already chosen a random word for you.");
 }
