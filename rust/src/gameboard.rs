460d90a583425bab35ae22defb9566129eb454aa
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-02-28 15:04:07 -0500

    Get game loop working properly

--- rust/src/gameboard.rs
+++ rust/src/gameboard.rs
@@ -22,75 +22,117 @@ pub struct GameState {
     pub guessed_letters: Vec<char>,
 }
 
-pub fn update_num_wrong(state: GameState) -> GameState {
-    let num_wrong = 
+pub fn guess_letter(state: &GameState) -> Vec<char> {
+    println!("Please enter a letter: ");
+    let mut input = String::new();
+    let letter =
+        match stdin().read_line(&mut input) {
+            Ok(_) => {
+                let result = input.trim();
+                if !result.is_empty() {
+                    let final_result =
+                        result
+                            .chars()
+                            .next()
+                            .unwrap()
+                            .to_ascii_lowercase();
+                    final_result
+                }
+                else {
+                    println!("Error getting input. Defaulting to 'a'");
+                    'a'
+                }
+            },
+            Err(err) => {
+                println!("Error reading input: {}. Defaulting to 'a'", err);
+                'a'
+            }
+        };
+    add_guess(letter, &state.guessed_letters)
+}
+
+fn add_guess(guess: char, current_guesses: &Vec<char>) -> Vec<char> {
+    let mut new_vec = current_guesses.clone();
+    new_vec.push(guess);
+    new_vec.dedup();
+    new_vec
+}
+
+fn get_hangman_state(num_wrong: u8) -> Hangman {
+    match num_wrong {
+        0 => Hangman::Empty,
+        1 => Hangman::Head,
+        2 => Hangman::Neck,
+        3 => Hangman::OneArm,
+        4 => Hangman::TwoArms,
+        5 => Hangman::OneLeg,
+        6 => Hangman::FinishedMan,
+        _ => Hangman::DeadMan,
+    }
+}
+
+pub fn check_num_wrong(state: &GameState) -> u8 {
+    let num_wrong =
         state.guessed_letters
             .iter()
             .filter(|x| !state.word.contains(**x))
             .collect::<Vec<&char>>()
             .len() as u8;
-    GameState {
-        wrong_guesses: num_wrong,
-        ..state
+    num_wrong
+}
+
+pub fn check_game_over(state: &GameState) -> bool {
+    let correct_guesses =
+        state.guessed_letters.len() - check_num_wrong(&state) as usize;
+    if state.wrong_guesses >= 7 {
+        println!("Game over. The word was {}", state.word);
+        true
+    }
+    else if correct_guesses >= state.word.len() {
+        println!("You win! The word was {}", state.word);
+        true
+    }
+    else {
+        false
     }
 }
 
-pub fn add_guess(state: GameState) -> GameState {
-    println!("Please enter a letter: ");
-    let mut temp = state.guessed_letters.clone();
-    let mut input = String::new();
-    match stdin().read_line(&mut input) {
-        Ok(_) => {
-            let result = input.trim();
-            if !result.is_empty() {
-                let final_result = 
-                    result
-                        .chars()
-                        .next()
-                        .unwrap()
-                        .to_ascii_lowercase();
-                temp.push(final_result);
-                GameState {
-                    guessed_letters: temp,
-                    ..state
-                }
-            }
-            else {
-                println!("Error getting input. Defaulting to 'a'");
-                temp.push('a');
-                GameState {
-                    guessed_letters: temp,
-                    ..state
-                }
-            }
-        },
-        Err(err) => {
-            println!("Error reading input: {}. Defaulting to 'a'", err);
-            temp.push('a');
-            GameState {
-                guessed_letters: temp,
-                ..state
-            }
-        }
+pub fn display_game_board(state: &GameState) {
+    let hangman = get_hangman_state(state.wrong_guesses);
+    let image = choose_game_image(hangman);
+    let char_vector =
+        state.word
+            .chars()
+            .map(|c|
+                if state.guessed_letters.contains(&c) { c }
+                else { '_' }
+            )
+            .collect::<Vec<char>>();
+    let mut result_string: String = String::new();
+    for c in char_vector {
+        result_string += c.to_string().as_str();
+        result_string += " ";
     }
+    result_string = result_string.trim_end().to_string();
+    println!("{}\n{}\n", image, result_string);
 }
 
-pub fn choose_game_image(state: Hangman) -> String {
+fn choose_game_image(state: Hangman) -> String {
     match state {
         Hangman::Empty       => String::from(hangman::EMPTY),
         Hangman::Head        => String::from(hangman::HEAD),
-        Hangman::Neck        => String::from(hangman::NECK), 
-        Hangman::OneArm      => String::from(hangman::ONE_ARM), 
-        Hangman::TwoArms     => String::from(hangman::TWO_ARMS), 
-        Hangman::OneLeg      => String::from(hangman::ONE_LEG), 
-        Hangman::FinishedMan => String::from(hangman::FINISHED_MAN), 
-        Hangman::DeadMan     => String::from(hangman::DEAD_MAN), 
+        Hangman::Neck        => String::from(hangman::NECK),
+        Hangman::OneArm      => String::from(hangman::ONE_ARM),
+        Hangman::TwoArms     => String::from(hangman::TWO_ARMS),
+        Hangman::OneLeg      => String::from(hangman::ONE_LEG),
+        Hangman::FinishedMan => String::from(hangman::FINISHED_MAN),
+        Hangman::DeadMan     => String::from(hangman::DEAD_MAN),
     }
 }
 
 pub fn choose_random_word() -> String {
     let path = "../words.txt";
-    let contents = 
+    let contents =
         match read_to_string(path) {
             Ok(file) => file,
             Err(err) => {
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,28 +1,34 @@
 mod hangman;
 mod gameboard;
-use gameboard::{GameState};
+use gameboard::{GameState, check_num_wrong, guess_letter};
 
 fn main() {
-    let random_word = gameboard::choose_random_word();
-    greet();
+    let initial_state = greet();
 
-    let initial_state = 
+    let mut state =
         GameState {
-            word: random_word,
+            word: initial_state.word,
             wrong_guesses: 0,
             guessed_letters: vec![],
         };
+
     // Game loop
-    loop {
-        let guessed_state = gameboard::add_guess(initial_state);
-        let updated_state = gameboard::update_num_wrong(guessed_state);
-
-        println!("{:?}", updated_state);
-
-        break;
+    while !gameboard::check_game_over(&state) {
+        state.guessed_letters = guess_letter(&state);
+        state.wrong_guesses = check_num_wrong(&state);
+        gameboard::display_game_board(&state);
     }
 }
 
-fn greet() {
+fn greet() -> GameState {
     println!("Welcome to Hangman! We have already chosen a random word for you.");
+    let random_word = gameboard::choose_random_word();
+    let initial_state =
+        GameState {
+            word: random_word,
+            wrong_guesses: 0,
+            guessed_letters: vec![]
+        };
+    gameboard::display_game_board(&initial_state);
+    initial_state
 }
