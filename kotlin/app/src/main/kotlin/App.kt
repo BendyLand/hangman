e41dc481cdd9f755e3a5b99cd8bb134f3d045d9e
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-04 14:43:25 -0500

    Finish Hangman in Kotlin

--- kotlin/app/src/main/kotlin/App.kt
+++ kotlin/app/src/main/kotlin/App.kt
@@ -9,17 +9,43 @@ object App {
         println("Welcome to Hangman! We have already chosen a random word for you.\n" +
                 "Let's start by choosing a letter: ")
     }
-    
+
     fun getRandomWord(): String {
-        val words = File("../../words.txt").useLines() { it.toList() }
+                         // I hate Gradle so much
+        val words = File("../../../../../words.txt").useLines() { it.toList() }
         val num = Random.nextInt(0, words.size)
         return words[num].lowercase()
     }
+
+    fun play() {
+        val choice = GameBoard.chooseLetter()
+        GameBoard.update(choice)
+        if (GameBoard.checkWin()) {
+            println("You win! The word was ${GameBoard.randomWord}")
+        }
+        else {
+            GameBoard.display()
+            if (GameBoard.numWrong < 7) {
+                play()
+            }
+        }
+    }
+
+    fun startGame() {
+        App.welcomePlayer(GameBoard.randomWord)
+        play()
+    }
 }
 
+/*  
+Run using: 
+
+kotlinc *.kt -include-runtime -d App.jar && kotlin App.jar 
+
+Gradle seems to breaks stdin, so the program will just close, regardless of how you gather input.
+Also, gradle doesn't like the recursive implementation. 
+*/
 fun main() {
-    App.welcomePlayer(GameBoard.randomWord)
+    App.startGame()
     println("The word is ${GameBoard.randomWord}")
-
-
 }
--- kotlin/app/src/main/kotlin/Gameboard.kt
+++ kotlin/app/src/main/kotlin/Gameboard.kt
@@ -14,6 +14,45 @@ object GameBoard {
             image + "\n" + "\t" + placeholder.joinToString(" ") + "\n"
     }
 
+    fun update(guess: Char) {
+        if (guessedChars.contains(guess) && !gameOver) {
+            println("Already guessed that letter!")
+        }
+        else {
+            guessedChars.add(guess)
+            val gameImage = chooseGameImage(checkNumWrong())
+            val resultWord = randomWord.indices.map { i: Int ->
+                if (guessedChars.contains(randomWord[i])) {
+                    randomWord[i].toString()
+                }
+                else {
+                    "_ "
+                }
+            }
+            gameboard = 
+                gameImage + "\n" + "\t" + resultWord.joinToString(" ") + "\n"
+        }
+    }
+
+    fun checkNumWrong(): Int {
+        val numIncorrectGuesses = 
+            guessedChars
+                .filter { c -> !randomWord.contains(c) }
+                .distinct()
+                .size
+        numWrong = numIncorrectGuesses
+        return numIncorrectGuesses
+    }
+
+    fun checkWin(): Boolean {
+        val numCorrectGuesses = 
+            guessedChars
+                .filter { c -> randomWord.contains(c) }
+                .distinct()
+                .size
+        return numCorrectGuesses == randomWord.toCharArray().distinct().size
+    }
+
     fun chooseGameImage(numWrong: Int): String {
         return when (numWrong) {
             0 -> Hangman.empty
@@ -29,6 +68,16 @@ object GameBoard {
                 println("The word was $randomWord")
                 Hangman.deadMan
             }
+        }
+    }
+
+    fun chooseLetter(): Char {
+        val input = readLine() ?: ""
+        return try {
+            input.trim().lowercase().first()
+        } 
+        catch (_: java.util.NoSuchElementException) {
+            'a'
         }
     }
 
