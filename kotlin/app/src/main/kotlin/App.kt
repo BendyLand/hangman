71bc45c63e7dfabbb535de1e9030ab1454ebfb0f
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-04 13:02:58 -0500

    Implement next few GameBoard methods

--- kotlin/app/src/main/kotlin/App.kt
+++ kotlin/app/src/main/kotlin/App.kt
@@ -1,16 +1,25 @@
 package bland.hangman
 
+import java.io.File
+import kotlin.random.*
+
 object App {
-    fun greetPlayer() {
+    fun welcomePlayer(randomWord: String) {
+        GameBoard.create(randomWord)
         println("Welcome to Hangman! We have already chosen a random word for you.\n" +
                 "Let's start by choosing a letter: ")
+    }
+    
+    fun getRandomWord(): String {
+        val words = File("../../words.txt").useLines() { it.toList() }
+        val num = Random.nextInt(0, words.size)
+        return words[num].lowercase()
     }
 }
 
 fun main() {
-    App.greetPlayer()
-    val word = GameBoard.getRandomWord()
-    GameBoard.create(word)
-    println(GameBoard.gameboard)
-    println("The word is $word")
+    App.welcomePlayer(GameBoard.randomWord)
+    println("The word is ${GameBoard.randomWord}")
+
+
 }
--- kotlin/app/src/main/kotlin/Gameboard.kt
+++ kotlin/app/src/main/kotlin/Gameboard.kt
@@ -1,21 +1,38 @@
 package bland.hangman
 
-import java.io.File
-import kotlin.random.*
-
 object GameBoard {
+    val randomWord = App.getRandomWord()
     var gameboard = ""
-
-    fun getRandomWord(): String {
-        val words = File("../../words.txt").useLines() { it.toList() }
-        val num = Random.nextInt(0, words.size)
-        return words[num].lowercase()
-    }
+    var gameOver = false
+    var numWrong = 0
+    var guessedChars: MutableList<Char> = mutableListOf()
 
     fun create(word: String): Unit {
         val image = Hangman.empty
         val placeholder = (1..word.length).map { "_ " }
         gameboard = 
             image + "\n" + "\t" + placeholder.joinToString(" ") + "\n"
+    }
+
+    fun chooseGameImage(numWrong: Int): String {
+        return when (numWrong) {
+            0 -> Hangman.empty
+            1 -> Hangman.head
+            2 -> Hangman.neck
+            3 -> Hangman.oneArm
+            4 -> Hangman.twoArms
+            5 -> Hangman.oneLeg
+            6 -> Hangman.finishedMan
+            else -> {
+                gameOver = true
+                println("Game over!")
+                println("The word was $randomWord")
+                Hangman.deadMan
+            }
+        }
+    }
+
+    fun display() {
+        println(gameboard)
     }
 }
